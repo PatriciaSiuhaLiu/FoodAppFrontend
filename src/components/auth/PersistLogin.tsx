a2584047f469377c0592159f552a1b2cb7cffbb1
Patricia Siuha Liu <liupatricia.18@gmail.com>
2023-03-14 10:38:44 -0400

    first commit

---
+++ src/components/auth/PersistLogin.tsx
@@ -0,0 +1,72 @@
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from 'axios';
+// import useRefreshToken from '../hooks/useRefreshToken';
+import useAuth from '../../hooks/useAuth';
+
+const PersistLogin = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const { auth, setAuthentication } = useAuth();
+    const getLoggedInUser = async () => {
+
+        try {
+            // Later change this to a passport auheticated server route like get user. This will 
+            //call the service with jwt if it is in cookie else if cookie not available
+            //it will go to error and set logged in false
+
+            // //decode token 
+            // const userDetail = jwt_decode()
+            // const url = process.env.ENV === "PROD" ? "https://online-food-order-nf2n.onrender.com" : "http://localhost:8000";
+            // const url = process.env.ENV === "DEV" ? "http://localhost:8000" : "https://online-food-order-nf2n.onrender.com";
+            const url = "https://online-food-order-nf2n.onrender.com";
+            // const response = await axios.get(`${url}/api/v1/logout`, {
+            const response = await axios.get(`${url}/api/v1/loggedInUser`,
+                {
+                    withCredentials: true
+                }
+
+            );
+            const { email, name, roles, token } = response.data.userData
+            setIsLoading(false)
+            const authData = {
+                email,
+                name,
+                roles,
+                token
+            }
+            setAuthentication(authData)
+
+
+        } catch (error) {
+            setAuthentication({
+                email: "",
+                name: "",
+                roles: [],
+                token: "",
+            })
+            console.log("Error----->", error);
+        }
+        finally {
+            setIsLoading(false)
+        }
+    }
+
+
+    useEffect(() => {
+        getLoggedInUser()
+    }, [])
+
+
+
+    return (
+        <>
+
+            {isLoading
+                ? <p>Loading...</p>
+                : <Outlet />}
+            {/* <Outlet /> */}
+        </>
+    )
+}
+
+export default PersistLogin
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,112 @@
+import { createContext, useState, ReactNode, useEffect } from 'react';
+import axios from 'axios';
+import jwt_decode from "jwt-decode";
+
+type AuthType = {
+    email: string;
+    name: string;
+    roles: Array<number>;
+    token: string
+}
+
+export type AuthContextType = {
+    isLoggedIn: boolean;
+    auth: {
+        email: string;
+        name: string;
+        roles: Array<number>;
+        token: string
+    },
+    // setAuth: (auth: AuthType) => void,
+    setAuthentication: (auth: AuthType) => void,
+    login: () => void;
+    logout: () => void;
+};
+const AuthContext = createContext<AuthContextType | null>(null);
+
+
+
+const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+    //On loading the appilication check the local storage
+
+    const callAPICheck = async () => {
+
+        try {
+            // Later change this to a passport auheticated server route like get user. This will 
+            //call the service with jwt if it is in cookie else if cookie not available
+            //it will go to error and set logged in false
+
+            // //decode token 
+            // const userDetail = jwt_decode()
+            // const url = process.env.ENV === "PROD" ? "https://online-food-order-nf2n.onrender.com" : "http://localhost:8000";
+            // const url = process.env.ENV === "DEV" ? "http://localhost:8000" : "https://online-food-order-nf2n.onrender.com";
+            const url = "https://online-food-order-nf2n.onrender.com";
+            // const response = await axios.get(`${url}/api/v1/logout`, {
+            const response = await axios.get(`${url}/api/v1/loggedInUser`,
+                {
+                    withCredentials: true
+                }
+
+            );
+            const { email, name, roles, token } = response.data.userData
+            const authData = {
+                email,
+                name,
+                roles,
+                token
+            }
+            setAuthentication(authData)
+
+
+        } catch (error) {
+            setAuthentication({
+                email: "",
+                name: "",
+                roles: [],
+                token: "",
+            })
+            console.log("Error----->", error);
+        }
+    }
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [auth, setAuth] = useState({
+        email: "",
+        name: "",
+        roles: [],
+        token: "",
+    } as AuthType);
+
+    useEffect(() => {
+
+        // callAPICheck();
+
+    }, []);
+
+    const setAuthentication = (auth: AuthType) => {
+        setAuth(auth)
+    }
+
+    const login = () => {
+
+        setIsLoggedIn(true);
+    };
+    const logout = () => {
+        setAuth({
+            email: "",
+            name: "",
+            roles: [],
+            token: "",
+        })
+        // setIsLoggedIn(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, login, logout, auth, setAuthentication }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
+export { AuthContextProvider };
